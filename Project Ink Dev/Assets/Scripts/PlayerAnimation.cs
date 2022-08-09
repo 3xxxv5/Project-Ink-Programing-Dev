39bb778ce51b0f5dc6cdfdf380f22a11fdad3466
WAITWAITWAIT <69075466+waitstar990611@users.noreply.github.com>
2022-08-09 09:16:45 +0800

    模型动画与朝向接口开放

--- Project Ink Dev/Assets/Scripts/PlayerAnimation.cs
+++ Project Ink Dev/Assets/Scripts/PlayerAnimation.cs
@@ -7,7 +7,7 @@ public class PlayerAnimation : MonoBehaviour
 
   [Header("转向速度")]
   [SerializeField]
-  private float turnSpeed = 20;
+  private float m_turnSpeed = 20;
   // 四元数移动模型朝向
   Quaternion m_Rotation = Quaternion.identity;
   // Start is called before the first frame update
@@ -22,23 +22,33 @@ public class PlayerAnimation : MonoBehaviour
   {
     m_Rigidbody.MoveRotation(m_Rotation);
   }
+  private void TargetPlayerRotation(Vector3 movement)
+  {
+    // 计算往目标朝向过度的矢量
+    Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, m_turnSpeed * Time.deltaTime, 0f);
+    m_Rotation = Quaternion.LookRotation(desiredForward);
+  }
   /// <summary>
   /// 动画进入移动或待机状态<para />
   /// <param name="movement">模型最终朝向向量<para /></param>
   /// <param name="isExitIdle">是否离开待机状态，true为移动，false为回到待机<para /></param>
   /// <param name="status">walk or jump or run<para /></param>
+  /// <returns>无返回</returns>
   /// </summary>
-  private void PlayerStatusChange(Vector3 movement, bool isExitIdle, string status)
+  public void PlayerStatusChange(Vector3 movement, bool isExitIdle, string status)
   {
     // 单位化方向向量
     movement.Normalize();
     // 播放status动画
     m_Animator.SetBool(status, isExitIdle);
   }
-  private void TargetPlayerRotation(Vector3 movement)
+  /// <summary>
+  /// 修改模型转向速度<para />
+  /// <param name="newSpeed">模型最终朝向向量<para /></param>
+  /// <returns>无返回</returns>
+  /// </summary>
+  public void TurnSpeedChange(float newSpeed)
   {
-    // 计算往目标朝向过度的矢量
-    Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, turnSpeed * Time.deltaTime, 0f);
-    m_Rotation = Quaternion.LookRotation(desiredForward);
+    m_turnSpeed = newSpeed;
   }
 }
