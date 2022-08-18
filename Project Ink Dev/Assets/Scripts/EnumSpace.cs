5feec87e1a8c2d54bccadb5b85e358711bf05429
WAITWAITWAIT <69075466+waitstar990611@users.noreply.github.com>
2022-08-19 01:17:24 +0800

    枚举状态，青蛙状态机

--- Project Ink Dev/Assets/Scripts/EnumSpace.cs
+++ Project Ink Dev/Assets/Scripts/EnumSpace.cs
@@ -1,19 +1,26 @@
-namespace EnumSpace{
-    public enum ItemType
-    {
-        Main,
-        Hidden
-    }
+namespace EnumSpace
+{
+  public enum ItemType
+  {
+    Main,
+    Hidden
+  }
 
-    public enum InteractiveType
-    {
-        Type1,  //按照设定轨迹移动的下落物，撞击一次即消失
-        Type2   //按照设定轨迹移动的下落物，撞击后分为4个部分，玩家依次撞击四个部分各一次，四个部分都消失视为撞击一次掉落物
-    }
+  public enum InteractiveType
+  {
+    Type1,  //按照设定轨迹移动的下落物，撞击一次即消失
+    Type2   //按照设定轨迹移动的下落物，撞击后分为4个部分，玩家依次撞击四个部分各一次，四个部分都消失视为撞击一次掉落物
+  }
 
-    public enum PlayStatus
-	{
-        Idle,
-        Dash
-	}
+  public enum PlayStatus
+  {
+    // 小于十是bool
+    Idle = 1,
+    Dash = 2,   // 虾独有
+    Walk = 3,
+    // 大于十是trigger
+    Charge = 11, // 蓄力青蛙独有
+    Launch = 12, // 发射青蛙独有
+  }
+
 }
--- Project Ink Dev/Assets/Scripts/PlayerAnimation.cs
+++ Project Ink Dev/Assets/Scripts/PlayerAnimation.cs
@@ -1,16 +1,17 @@
 using UnityEngine;
+using EnumSpace;
 
 public class PlayerAnimation : MonoBehaviour
 {
   //Rigidbody m_Rigidbody;
   Animator m_Animator;
 
-//   [Header("转向速度")]
-//   [SerializeField]
-//   private float m_turnSpeed = 20;
+  //   [Header("转向速度")]
+  //   [SerializeField]
+  //   private float m_turnSpeed = 20;
   // 四元数移动模型朝向
   // private Quaternion m_Rotation = Quaternion.identity;
-  private string m_nowStatus = "idle";
+  private PlayStatus m_nowStatus = PlayStatus.Idle;
   // Start is called before the first frame update
   void Start()
   {
@@ -23,40 +24,40 @@ public class PlayerAnimation : MonoBehaviour
   //{
   //  m_Rigidbody.MoveRotation(m_Rotation);
   //}
-//   private void TargetPlayerRotation(Vector3 movement)
-//   {
-//     // 计算往目标朝向过度的矢量
-//     Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, m_turnSpeed * Time.deltaTime, 0f);
-//     m_Rotation = Quaternion.LookRotation(desiredForward);
-//   }
+  //   private void TargetPlayerRotation(Vector3 movement)
+  //   {
+  //     // 计算往目标朝向过度的矢量
+  //     Vector3 desiredForward = Vector3.RotateTowards(transform.forward, movement, m_turnSpeed * Time.deltaTime, 0f);
+  //     m_Rotation = Quaternion.LookRotation(desiredForward);
+  //   }
   /// <summary>
   /// 动画进入移动或待机状态<para />
-  /// <param name="status">walk or jump or run(run is not exist)<para /></param>
+  /// <param name="status">见PlayStatus<para /></param>
   /// <returns>无返回</returns>
   /// </summary>
-  public void PlayerStatusChange(string status)
+  public void PlayerStatusChange(PlayStatus status)
   {
     // 单位化方向向量
     // movement.Normalize();
     // 播放status动画
-    m_Animator.SetBool(status, true);
+    m_Animator.SetBool(status.ToString(), true);
     // 记录当前状态
     m_nowStatus = status;
     //TargetPlayerRotation(movement);
   }
   public void PlayerReturnIdle()
   {
-    // 播放idle动画
-    m_Animator.SetBool(m_nowStatus, false);
-    m_nowStatus = "idle";
+    // 播放Idle动画
+    m_Animator.SetBool(m_nowStatus.ToString(), false);
+    m_nowStatus = PlayStatus.Idle;
   }
   /// <summary>
   /// 修改模型转向速度<para />
   /// <param name="newSpeed">模型最终朝向向量<para /></param>
   /// <returns>无返回</returns>
   /// </summary>
-//   public void TurnSpeedChange(float newSpeed)
-//   {
-//     m_turnSpeed = newSpeed;
-//   }
+  //   public void TurnSpeedChange(float newSpeed)
+  //   {
+  //     m_turnSpeed = newSpeed;
+  //   }
 }
