25540631bac9abf5d7effc17c50867e65d661635
WAITWAITWAIT <69075466+waitstar990611@users.noreply.github.com>
2022-08-22 01:42:49 +0800

    收集花瓣

--- Project Ink Dev/Assets/Scripts/UserInterface.cs
+++ Project Ink Dev/Assets/Scripts/UserInterface.cs
@@ -6,11 +6,25 @@ public class UserInterface : MonoBehaviour
 {
   private TextMeshProUGUI CurrentScore;
   private TextMeshProUGUI TargetScore;
+  private GameObject[] ImageList;
+  private Sprite collectionFull;
+  private Sprite collectionEmpty;
 
   private void Awake()
   {
-        CurrentScore = GameObject.Find("Canvas/Text[CurrentScore]").GetComponent<TextMeshProUGUI>();
-        TargetScore = GameObject.Find("Canvas/Text[TargetScore]").GetComponent<TextMeshProUGUI>();
+    CurrentScore = GameObject.Find("Canvas/Text[CurrentScore]").GetComponent<TextMeshProUGUI>();
+    TargetScore = GameObject.Find("Canvas/Text[TargetScore]").GetComponent<TextMeshProUGUI>();
+    collectionFull = Resources.Load<Sprite>("Image/collection_full");
+    collectionEmpty = Resources.Load<Sprite>("Image/collection_empty");
+    ImageList = GameObject.FindGameObjectsWithTag("Collections");
+  }
+  private void Start()
+  {
+    foreach (GameObject image in ImageList)
+    {
+      Debug.Log("test");
+      image.GetComponent<Image>().sprite = collectionEmpty;
+    }
   }
   // private void Update()
   // {
@@ -20,5 +34,6 @@ public class UserInterface : MonoBehaviour
   {
     CurrentScore.text = "Current:" + cScore.ToString();
     TargetScore.text = "total:" + tScore.ToString();
+    ImageList[cScore - 1].GetComponent<Image>().sprite = collectionFull;
   }
 }
