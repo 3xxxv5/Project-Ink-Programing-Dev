4f5ca11a56a6799c396ff50ef2b720e826f883c7
hd233yui <1017432382@qq.com>
2022-08-21 17:49:37 +0800

    资源添加及准心替换

--- Project Ink Dev/Assets/Scripts/RotateItem.cs
+++ Project Ink Dev/Assets/Scripts/RotateItem.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class RotateItem : PowerFlower1
 {
-    Rigidbody rb;
+    Rigidbody rb1;
     Transform ts;
     [Header("旋转轴相对物体位置")]
     public Vector3 pivot = new Vector3(2, 0, 0);
@@ -14,18 +14,18 @@ public class RotateItem : PowerFlower1
     void OnEnable()
     {
         init();
-        rb = GetComponent<Rigidbody>();
+        rb1 = GetComponent<Rigidbody>();
         ts = GetComponent<Transform>();
-        rb.useGravity = false;
-        rb.velocity = new Vector3(0f, -m_velocity, 0f);
+        rb1.useGravity = false;
+        rb1.velocity = new Vector3(0f, -m_velocity, 0f);
         pivot += ts.position;
     }
 
     public override void SetFallingTrack()
     {
-        var m = rb.mass;
+        var m = rb1.mass;
         transform.RotateAround(pivot, Vector3.down, rotateSpeed);
         Vector3 force = new Vector3(0f, -m * m_acceleratetion, 0f);
-        rb.AddForce(force);
+        rb1.AddForce(force);
     }
 }
