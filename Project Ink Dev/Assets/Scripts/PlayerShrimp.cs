f617e9fd5f5aae570312175b78721a3d6d4653c0
3xxxv5 <1379652041@qq.com>
2022-08-23 21:44:18 +0800

    player相关代码优化

---
+++ Project Ink Dev/Assets/Scripts/PlayerShrimp.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using DG.Tweening;
+
+
+public class PlayerShrimp : BasePlayer
+{
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+        if (moveStatus != EnumSpace.PlayStatus.Faint)
+        {
+            PlayerMove();
+        }
+            
+        //更新cd
+        CheckIsInCD();
+    }
+
+    protected override void MouseClick()
+    {
+        //点击鼠标左键加速冲刺
+        if (Input.GetMouseButtonDown(0) && beginCD == false)
+        {
+            //Vector3 localPos = characterTran.localPosition;
+            //localPos.z += dashDis;
+            //Vector3 pos = playerTran.TransformPoint(localPos);
+            //playerTran.DOMove(pos, lastTime);
+            //playerTran.DOMove(new Vector3(pos.x, pos.y, pos.z + dashDis), lastTime);
+            LetMove();
+            beginCD = true;
+            cd = 1;
+
+            //播放冲刺动画
+            characterGO.GetComponent<ShrimpAnimator>().Dash();
+            SetMoveStatus(EnumSpace.PlayStatus.Dash);
+            Sequence seq = DOTween.Sequence();
+            seq.AppendInterval(LAST_TIME);
+            seq.AppendCallback(SetStatusToIdle);
+        }
+    }
+
+    protected override void PlayWalkAnim()
+    {
+        characterGO.GetComponent<ShrimpAnimator>().Walk();
+    }
+
+    protected override void PlayIdleAnim()
+    {
+        characterGO.GetComponent<ShrimpAnimator>().Idle();
+    }
+}
