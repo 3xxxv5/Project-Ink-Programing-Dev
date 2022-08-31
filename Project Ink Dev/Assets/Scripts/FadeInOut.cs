e0ecf1a8baab8bc5b6d4578b17851e019ad3d61d
WAITWAITWAIT <69075466+waitstar990611@users.noreply.github.com>
2022-09-01 04:03:39 +0800

    剧情配置，场景渐入

--- Project Ink Dev/Assets/Scripts/FadeInOut.cs
+++ Project Ink Dev/Assets/Scripts/FadeInOut.cs
@@ -2,9 +2,11 @@ using UnityEngine;
 using UnityEngine.UI;
 public class FadeInOut : MonoBehaviour
 {
+  [SerializeField]
+  private float fadeDuration = 1;
   void Start()
   {
     Image fade = gameObject.GetComponent<Image>();
-    fade.CrossFadeAlpha(0, 1, false);
+    fade.CrossFadeAlpha(0, fadeDuration, true);
   }
 }
