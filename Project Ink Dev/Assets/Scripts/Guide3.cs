6c6598e0b63053ecb9e36e6f60532764d29280ba
Tabris <emilio970211@163.com>
2022-09-01 17:42:44 +0800

    1

--- Project Ink Dev/Assets/Scripts/Guide3.cs
+++ Project Ink Dev/Assets/Scripts/Guide3.cs
@@ -5,26 +5,62 @@ using TMPro;
 
 public class Guide3 : MonoBehaviour
 {
+    private EnumSpace.GuideStep guideStep;
     public GameObject player;
     public TextMeshProUGUI Text;
 
     void OnEnable()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        string content = "<rotate=90>鹤眼：化身仙鹤后，可长按鼠标右键增加瞄准时间";
+        guideStep = EnumSpace.GuideStep.Step1;
+        string content = "<rotate=90>彼岸花会分裂，撞击所有部分来收集";
         Text.text = content;
         Debug.Log(Text.text);
         Guide3MesManager.Instance.guideStatus = EnumSpace.GuideStatus.InGuide;
+        player.GetComponent<PlayerCrane>().enabled = false;
         player.GetComponent<ThirdPersonCamera>().enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetMouseButton(1))
+        switch (guideStep)
+        {
+            case EnumSpace.GuideStep.Step1:
+                Step1();
+                break;
+            case EnumSpace.GuideStep.Step2:
+                Step2();
+                break;
+        }
+        
+    }
+
+    private void Step1()
+    {
+        StartCoroutine("ShowText");
+    }
+
+    IEnumerator ShowText()
+    {
+        float t = 0;
+        while(t < 1f)
+        {
+            t += Time.unscaledDeltaTime / 5f;
+            yield return null;
+        }
+        string content = "<rotate=90>鹤眼：化身仙鹤后，可长按鼠标右键增加瞄准时间";
+        Text.text = content;
+        guideStep = EnumSpace.GuideStep.Step2;
+    }
+
+    private void Step2()
+    {
+        if (Input.GetMouseButton(1))
         {
             Destroy(Text.gameObject);
             //Debug.Log(Text.text);
+            player.GetComponent<PlayerCrane>().enabled = true;
             player.GetComponent<ThirdPersonCamera>().enabled = true;
             Guide3MesManager.Instance.guideStatus = EnumSpace.GuideStatus.OutGuide;
             Destroy(this.gameObject);
